b615d9f6ca0956f82927711bd45d4508c731db9b
Tony Wang <Tony94042@gmail.com>
2023-12-13 17:03:16 +1100

    Started the weather service logic

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http'
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
 
-  constructor() { }
+  constructor(){}
+
+  getForcast(){
+    this.getCurrentLocation()
+      .pipe(
+        map(coords => {
+          return new HttpParams()
+            .set('lat', String(coords.latitude))
+            .set('lon', String(coords.longitude))
+            .set('units', 'metric')
+            .set('appid', '576f05572e3ddbe8b9d5d42c100f3291')
+        })
+      )
+  }
+
+  getCurrentLocation(){
+    return new Observable<GeolocationCoordinates>((observer) => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete();
+        },
+        (err) => observer.error(err)
+      );
+    })
+  }
 }
