b13d43a578c37025f567f36943036022aa82122f
Tony Wang <Tony94042@gmail.com>
2023-12-22 11:54:10 +1100

    Started Services Logic

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
+  messages: Subject<any>;
 
-  constructor() { }
+  constructor(){
+    this.messages = new Subject<any>
+  }
+
+  addSuccess(message: string){
+    this.messages.next(message);
+
+  }
+
+  addError(message: string){
+
+  }
+
+  clearMessage(id: number){
+
+  }
 }
 
 //Behaviour Subjects, Async Subjects, Replay Subjects
