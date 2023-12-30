f6fd8111a4a08f64c4107ce06754d5ffe78e9f92
Tony Wang <Tony94042@gmail.com>
2023-12-30 12:48:22 +1100

    More Logic for API services

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import {tap, map, switchMap } from  'rxjs/operators'
+import { HttpParams, HttpClient } from '@angular/common/http';
+
+interface NewsApiResponse {
+  totalResults: number;
+  articles: {
+    title: string;
+    url: string;
+  }[]
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +20,24 @@ export class NewsApiService {
   private apiKey = 'd2cc59f8f5734a77acd4f59603df3a5f';
   private country = 'au';
 
-  constructor() { }
+  pagesInput: Subject<number>;
+  pagesOutput: Observable<any>
+  numberOfPages: Observable <number>;
+
+  constructor(private http: HttpClient) {
+    this.pagesInput = new Subject();
+    this.pagesOutput = this.pagesInput.pipe(
+      map((page) => {
+        return new HttpParams()
+          .set('apiKey', this.apiKey)
+          .set('country', this.country)
+          .set('pageSize', this.pageSize)
+          .set('page', page);  
+      }),
+      switchMap((params) => {
+        return this.http.get<NewsApiResponse>(this.url, {params} )
+      })
+    )
+
+  }
 }
